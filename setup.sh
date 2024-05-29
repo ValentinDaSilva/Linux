87ec795b08b3fd537480efb0ebc96ce7a4978a1b
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2024-05-29 14:51:55 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,18 +11,25 @@ apt-get update
 echo "Instalando wget y otros paquetes necesarios..."
 apt-get install -y wget
 
-# Descargar el archivo .deb de Visual Studio Code
-echo "Descargando Visual Studio Code..."
-wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/code_1.89.1-1715060508_amd64.deb" -O /tmp/code_1.89.1-1715060508_amd64.deb
+#!/bin/bash
 
-# Instalar el archivo .deb de Visual Studio Code
-echo "Instalando Visual Studio Code..."
-dpkg -i /tmp/code_1.89.1-1715060508_amd64.deb
+# Preguntar al usuario si desea descargar Visual Studio Code
+read -p "¿Desea descargar Visual Studio Code? [Y/N]: " respuesta
 
-# Corregir dependencias si es necesario
-echo "Corrigiendo dependencias..."
-apt-get install -f -y
+# Convertir la respuesta a minúsculas para manejar Y/y y N/n
+respuesta=$(echo "$respuesta" | tr '[:upper:]' '[:lower:]')
 
+# Verificar la respuesta del usuario
+if [[ "$respuesta" == "y" || "$respuesta" == "yes" ]]; then
+    echo "Descargando Visual Studio Code..."
+    wget "https://vscode.download.prss.microsoft.com/dbazure/download/stable/dc96b837cf6bb4af9cd736aa3af08cf8279f7685/code_1.89.1-1715060508_amd64.deb" -O /tmp/code_1.89.1-1715060508_amd64.deb
+    echo "Instalando Visual Studio Code..."
+    sudo dpkg -i /tmp/code_1.89.1-1715060508_amd64.deb
+    sudo apt-get install -f -y  # Para corregir posibles dependencias faltantes
+    echo "Visual Studio Code ha sido instalado."
+else
+    echo "No se descargará Visual Studio Code."
+fi
 # Limpieza
 echo "Limpiando..."
 apt-get clean
