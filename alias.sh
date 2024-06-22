6d2950af7afdd876d3a736639f4cf308dc3cb5e4
Valentin <67626546+ValentinDaSilva@users.noreply.github.com>
2024-06-22 01:27:20 -0300

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 echo 'alias cl="clear"' >> ~/.bashrc
+echo 'alias push="git add . && git commit -m \"Commit automatico\" && git push origin main"' >> ~/.bashrc
 echo 'alias logisim="/opt/logisim-evolution/bin/logisim-evolution"' >> ~/.bashrc
 source ~/.bashrc
